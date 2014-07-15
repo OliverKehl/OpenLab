a407733bc67457434d36f776aca66b0fbc12df07
kehl <kangjihua_home@126.com>
2014-07-15 15:16:21 +0800

    aaa

---
+++ src/webpy/rest.py
@@ -0,0 +1,19 @@
+import web
+import json
+
+urls = ('/(.*)', 'index')
+class index:
+    def GET(self,tag):
+        #do some thingi 
+        web.input(tag=None)
+        if tag=='python':
+            pyDict = {'hostname':'khost','username':'kangjihua','password':'opentech','port':'8080'}
+            web.header('Content-Type', 'application/json')
+            return json.dumps(pyDict)
+        else:
+            return 'Hello world'
+        
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
