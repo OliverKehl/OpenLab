abdb3a5a1e65922f181ec18d6cb369a8cc5c9219
kehl <kangjihua_home@126.com>
2014-07-15 11:02:17 +0800

    follow up

--- src/azure/AzureService.py
+++ src/azure/AzureService.py
@@ -1,10 +1,14 @@
-from azure import *
+from azure import (
+    WindowsAzureError,
+    MANAGEMENT_HOST,
+    _validate_not_none,               
+    )
 from azure.servicemanagement import *
 
 subscription_id = '0db41f26-7fe8-4402-a13b-9efc38d25f14'
 cert_file = '/home/kehl/mycert.pem'
 class AzureService():
-    def __init__(self,subscription_id=None, cert_file=None, host='management.core.chinacloudapi.cn'):
+    def __init__(self,subscription_id=None, cert_file=None, host=MANAGEMENT_HOST):#'management.core.chinacloudapi.cn'
         '''
             constructor
         '''
@@ -12,9 +16,20 @@ class AzureService():
         self.connected=True
     
     def create_virtual_machine(self,service_name,deployment_name):
-        #judge whether the cloud service exist or not
-        self.sms.ge
-         
-        #self.sms.create_virtual_machine_deployment(service_name, deployment_name, deployment_slot, label, role_name, system_config, os_virtual_hard_disk, network_config, availability_set_name, data_virtual_hard_disks, role_size, role_type, virtual_network_name)
+        if self.connected==False:
+            raise WindowsAzureError('')
+        try:
+            #judge whether the cloud service exist or not
+            hosted_services = self.sms.list_hosted_services()
+            for service in hosted_services:
+                if service_name==service.service_name:
+                    break
+                else:
+                    pass
+                    
+             
+            #self.sms.create_virtual_machine_deployment(service_name, deployment_name, deployment_slot, label, role_name, system_config, os_virtual_hard_disk, network_config, availability_set_name, data_virtual_hard_disks, role_size, role_type, virtual_network_name)
+        except Exception,e:
+            pass
     
     
