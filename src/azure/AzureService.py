db354d2cd656c3e66721d284551cde6e20f304ad
kehl <kangjihua_home@126.com>
2014-07-15 10:14:47 +0800

    update

--- src/azure/AzureService.py
+++ src/azure/AzureService.py
@@ -8,7 +8,13 @@ class AzureService():
         '''
             constructor
         '''
-        sms=ServiceManagementService(subscription_id,cert_file)
+        self.sms=ServiceManagementService(subscription_id,cert_file)
+        self.connected=True
     
+    def create_virtual_machine(self,service_name,deployment_name):
+        #judge whether the cloud service exist or not
+        self.sms.ge
+         
+        #self.sms.create_virtual_machine_deployment(service_name, deployment_name, deployment_slot, label, role_name, system_config, os_virtual_hard_disk, network_config, availability_set_name, data_virtual_hard_disks, role_size, role_type, virtual_network_name)
     
     
