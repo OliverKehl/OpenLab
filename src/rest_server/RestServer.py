065158f3511d1834e7503b5c7cf4c5da8f9fe196
kehl <kangjihua_home@126.com>
2014-07-15 17:21:47 +0800

    asddd

---
+++ src/rest_server/RestServer.py
@@ -0,0 +1,47 @@
+import paramiko
+import os
+
+
+class RestServer():
+    def __init__(self):
+        self.labs=[]
+        self.virtual_machines=[]
+        self.lab_vm_table = {}
+        self._get_lab_vm_table()
+        
+    def _get_lab_vm_table(self):
+        #current_dir = os.getcwd()
+        os.chdir('../../conf')
+        f = open(os.getcwd()+'/lab_vm.conf','r')
+        self.labs.append(f.readline())
+        self.virtual_machines.append(f.readline())
+     
+    def resource_monitor(self,host,username,password,port):
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(host, port, username, password);
+            return client
+        except Exception, e:
+            print 'Error Connecting : '+str(e)
+        
+
+def connect(hostname,port=22,username=None,password=None):
+    
+
+def test():
+    client = connect('kangjihua.chinacloudapp.cn',22,'kangjihua','oliverkahnno.1');
+    stdin,stdout,stderr = client.exec_command('sudo find / -name \.vnc')
+    #stdin.write('oliverkahnno.1\n')
+    #stdin.flush()
+    print stdout.read()
+    print stderr.read()
+    client.close()
+    
+if __name__=='__main__':
+    rs = RestServer()
+    print rs.labs
+    print rs.virtual_machines
+    
+    
+    
--- src/webpy/rest.py
+++ src/webpy/rest.py
@@ -1,17 +1,20 @@
 import web,json
-
+from azurecli import *
+from rest_server import *
 urls = ('/(.*)', 'index')
 class index:
     def GET(self,tag):
         #do some thingi 
         web.input(tag=None)
         if tag=='python':
+            vm_list = get_
+            get_virtual_machine_status()
             pyDict = {'hostname':'khost','username':'kangjihua','password':'opentech','port':'8080'}
             web.header('Content-Type', 'application/json')
             return json.dumps(pyDict)
         else:
             return 'Hello world'
-        
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
